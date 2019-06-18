4ec472b8dea949596930f8be5c8c2d672688ff2a
TonyKalo <tonikalogjera@gmail.com>
2019-06-18 09:09:55 +0200

    created branches

--- app/src/main/java/com/example/template/data/DataManager.kt
+++ app/src/main/java/com/example/template/data/DataManager.kt
@@ -1,8 +1,6 @@
 package com.example.template.data
 
 import com.example.template.data.db.AppDatabase
-import com.example.template.data.db.Database
-import com.example.template.data.db.dao.UsersDao
 
 interface DataManager{
 
