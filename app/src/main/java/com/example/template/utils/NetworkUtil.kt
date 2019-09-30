539caee19f7116ee594b276cbef2706fd370e8a8
TonyKalo <tonikalogjera@gmail.com>
2019-09-30 07:23:56 +0200

    Fix: NetworkUtil to class

--- app/src/main/java/com/example/template/utils/NetworkUtil.kt
+++ app/src/main/java/com/example/template/utils/NetworkUtil.kt
@@ -3,8 +3,14 @@ package com.example.template.utils
 import android.content.Context
 import android.net.ConnectivityManager
 
-fun isNetworkConnected(from: Context?): Boolean = from?.let {
-    val manager = from.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    val activeInfo = manager.activeNetworkInfo
-    activeInfo != null && activeInfo.isConnectedOrConnecting
-} ?: false
+class NetworkUtil {
+    companion object {
+        fun isNetworkConnected(from: Context?): Boolean = from?.let {
+            val manager = from.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val activeInfo = manager.activeNetworkInfo
+            activeInfo != null && activeInfo.isConnectedOrConnecting
+        } ?: false
+
+
+    }
+}
